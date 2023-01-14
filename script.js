5614e5b869da862843140048925d9321f35999fb
Raptor7742 <106883599+Raptor7742@users.noreply.github.com>
2023-01-14 15:09:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ const playerInstance = jwplayer("player").setup({
       sources: [
         {
           file:
-            "",
+            "https://dl.videas.fr/v-medias/s3/uploaded/%C3%A9pisode_10.mp4?fileName=episode-10mp4-source.mp4",
           label: "1080p",
           default: true
         },
@@ -42,7 +42,7 @@ const playerInstance = jwplayer("player").setup({
         },
         {
           file:
-            "",
+            "https://dl.videas.fr/v-medias/s3/a/34/14/3414e5d20a2449048338e3dfef72a6a1/d3a30d9d30eb491aad25916f8af1a051/e6b59115-ad86-4ed1-8754-440a59085967.mp4?fileName=episode-10mp4-480p.mp4",
           label: "480p"
         },
         {
